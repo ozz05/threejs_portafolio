a2aa9fe0974b9a65f7bcfae25323ca6aa54948ad
OzzSilva <osvaldosilva5@outlook.com>
2025-06-08 11:50:51 -0600

    Added Navbar and Hero basic with 3D Object

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Navbar from "./sections/Navbar.jsx";
+import Hero from "./sections/Hero.jsx";
 
 const App = () => {
     return (
         <main className={"max-w-7xl mx-auto"}>
-            <h1 className={"text-2xl text-white underline"}> Hello, Three.js bitch!</h1>
+            <Navbar/>
+
+            <Hero/>
         </main>
     )
 }
