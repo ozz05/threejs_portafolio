3a754091994fcdbadc4d714a9d18ee21c4d6aa66
OzzSilva <osvaldosilva5@outlook.com>
2025-06-10 12:33:31 -0600

    Added settings

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = 'threejs_portafolio';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
+    base: `/${repoName}/`,
+    plugins: [
       react(),
   ],
 })
